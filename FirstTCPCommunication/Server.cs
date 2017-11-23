c65ddfce9f3aaf781cc65fdb8c555c3cfa11b2c2
Shady M <hakeem2008@hotmail.de>
2017-11-23 19:01:50 +0100

    Start

---
+++ FirstTCPCommunication/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTCPCommunication
+{
+    class Server
+    {
+        Socket serverSocket;
+
+        public Server()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        }
+    }
+}
