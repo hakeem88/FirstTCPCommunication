5f9b42da41b60d223926da1f94a6227acdc6b45e
Shady M <hakeem2008@hotmail.de>
2017-11-23 19:06:49 +0100

    server constructor implemented

--- FirstTCPCommunication/Server.cs
+++ FirstTCPCommunication/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +14,9 @@ namespace FirstTCPCommunication
 
         public Server()
         {
-            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); //Adressfamilie IP4 oder IP6, TCP etc..
+            serverSocket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8090)); // IP & PORT
+            serverSocket.Listen(10); //Anzahl der Connections in der Queue 
         }
     }
 }
